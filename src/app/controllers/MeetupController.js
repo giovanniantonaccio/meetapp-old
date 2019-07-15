2dde6bb77cd8a90970a82b90e7ede2c72ae94bff
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-15 17:56:30 -0400

    Add new meetup
    Adding the following changes:
    
    - Adding Meetup controller, model and migration.
    - Adding association to models.
    - Removing tmp folder from git.
    - Adding date-fns to manipulate dates.

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Meetup from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      banner_id: Yup.number(),
+      user_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { name, description, location, date, banner_id } = req.body;
+
+    /**
+     * Check if date is in the past
+     */
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const meetup = await Meetup.create({
+      name,
+      description,
+      location,
+      date: hourStart,
+      banner_id,
+      user_id: req.userId,
+    });
+
+    return res.json(meetup);
+  }
+}
+
+export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.use(authMiddleware);
 
 // Routes that require authentication go here.
 routes.put('/users', UserController.update);
+
+routes.post('/meetups', MeetupController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
