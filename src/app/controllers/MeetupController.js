d9f5aaf6cb0a8829eab5006123bd5aaf82596e8e
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-07-16 10:30:24 -0400

    Add show, update and delete routes for meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,8 +1,37 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
+import File from '../models/File';
+import User from '../models/User';
 
 class MeetupController {
+  async index(req, res) {
+    const { page } = req.query;
+    const pagination_limit = 10;
+
+    const meetups = await Meetup.findAll({
+      where: { user_id: req.userId },
+      order: ['date'],
+      limit: pagination_limit,
+      offset: (page - 1) * pagination_limit,
+      attributes: ['id', 'name', 'description', 'location', 'date'],
+      include: [
+        {
+          model: File,
+          as: 'banner',
+          attributes: ['id', 'path', 'url'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name', 'email'],
+        },
+      ],
+    });
+
+    return res.json(meetups);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -37,6 +66,94 @@ class MeetupController {
     });
 
     return res.json(meetup);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name, description, location, date, banner_id } = req.body;
+    /**
+     * Check if the meetup exist
+     */
+    const meetup = await Meetup.findByPk(id);
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup does not exist' });
+    }
+
+    /**
+     * Check if the user is the owner of the meetup
+     */
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({ error: 'User is not owner of the meetup' });
+    }
+
+    /**
+     * Check if the meetup is in the past
+     */
+    if (isBefore(meetup.date, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'Cannot update meetups that already occured' });
+    }
+
+    /**
+     * Check if the banner exists
+     */
+    const bannerExists = await File.findByPk(banner_id);
+    if (banner_id && !bannerExists) {
+      return res.status(400).json({ error: 'Banner does not exist' });
+    }
+
+    /**
+     * Check if date is in the past
+     */
+    const hourStart = startOfHour(parseISO(date));
+    if (date && isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Cannot update for a past date' });
+    }
+
+    const updatedMeetup = await meetup.update({
+      name,
+      description,
+      location,
+      date: hourStart,
+      banner_id,
+    });
+
+    return res.json(updatedMeetup);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    /**
+     * Check if the meetup exist
+     */
+    const meetup = await Meetup.findByPk(id);
+
+    if (!meetup) {
+      return res.status(400).json({ error: 'Meetup does not exist' });
+    }
+
+    /**
+     * Check if the user is the owner of the meetup
+     */
+    if (meetup.user_id !== req.userId) {
+      return res.status(401).json({ error: 'User is not owner of the meetup' });
+    }
+
+    /**
+     * Check if the meetup is in the past
+     */
+    if (isBefore(meetup.date, new Date())) {
+      return res
+        .status(400)
+        .json({ error: 'Cannot delete meetups that already occured' });
+    }
+
+    await Meetup.destroy({ where: { id } });
+
+    return res.json();
   }
 }
 
